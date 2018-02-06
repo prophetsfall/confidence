85e5f4134b90ef07438c9bedcc4bc3f780f1911c
Patrick Whelan <patrickawhelan@gmail.com>
2018-02-06 16:27:59 -0500

    Seeds for week data

--- spec/features/user_creates_a_league_spec.rb
+++ spec/features/user_creates_a_league_spec.rb
@@ -20,6 +20,25 @@ feature "as an authenticated user I be able to create a league " do
     click_link "Click here to create a custom league!"
     fill_in "League name", with: "This is a custom league"
     fill_in "Max members", with: 10
+    choose "public-league-button"
+    click_button "Create League"
+
+    expect(page).to have_content "League Created Successfully!"
+    expect(page).to have_content "League Homepage for This is a custom league"
+    expect(page).to have_content user1.username
+    expect(page).to have_no_content user2.username
+  end
+  scenario "User creates a public league" do
+    visit "/"
+    click_link "Registered users click here to sign in!"
+    fill_in "Email", with: user1.email
+    fill_in "Password", with: user1.password
+    click_button "Log in"
+
+    click_link "Click here to create a custom league!"
+    fill_in "League name", with: "This is a custom league"
+    fill_in "Max members", with: 10
+    choose "private-league-button"
     click_button "Create League"
 
     expect(page).to have_content "League Created Successfully!"
