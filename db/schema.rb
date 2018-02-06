167417c584890cde08c0b2c16471ae1efaa4ec10
Patrick Whelan <patrickawhelan@gmail.com>
2018-02-06 15:20:16 -0500

    with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205182419) do
+ActiveRecord::Schema.define(version: 20180206142343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180205182419) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "max_members", default: 100, null: false
+    t.integer "invite_only", default: 2, null: false
     t.index ["league_name"], name: "index_leagues_on_league_name", unique: true
   end
 
